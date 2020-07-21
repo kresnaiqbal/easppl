43f739aa8d11725c4515bf631dd2dc7e30976837
Chiibee <ichwanlatiffajari@gmail.com>
2020-07-21 11:42:42 +0700

    login fix

--- connectdb.php
+++ connectdb.php
@@ -2,7 +2,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $dbname = "tubes";
+    $dbname = "koperasi";
 
     //create connection to database
     $conn = mysqli_connect($servername, $username, $password, $dbname);
--- validasi_user.php
+++ validasi_user.php
@@ -6,14 +6,14 @@
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-    $sql = mysqli_query($conn, "SELECT * FROM anggota where username = '$username' and password = '$password' and id = '$id'");
+    $sql = mysqli_query($conn, "SELECT * FROM anggota where username = '$username' and password = '$password'");
     $login = mysqli_num_rows($sql);
 
     if($login)
     {
         session_start();
         $_SESSION["username"] = $username;
-        header("location:easppl/template.php");
+        header("location:template.php");
     }
     else
     {
