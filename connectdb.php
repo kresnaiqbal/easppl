696e9f8db1187e4052746a951ecf5fc72ca96e5d
Chiibee <ichwanlatiffajari@gmail.com>
2020-07-21 14:04:08 +0700

    ichwan cek

--- connectdb.php
+++ connectdb.php
@@ -2,7 +2,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $dbname = "dbeas";
+    $dbname = "koperasi";
 
     //create connection to database
     $conn = mysqli_connect($servername, $username, $password, $dbname);
